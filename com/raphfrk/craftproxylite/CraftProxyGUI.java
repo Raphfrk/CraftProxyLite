6edf783c4b69ca15ef10b5c3d76085f78bd97dbd
Raphfrk <raphfrk@gmail.com>
2011-05-04 00:10:54 +0100

    Added storage of settings on GUI

--- com/raphfrk/craftproxylite/CraftProxyGUI.java
+++ com/raphfrk/craftproxylite/CraftProxyGUI.java
@@ -26,75 +26,88 @@ public class CraftProxyGUI extends JFrame implements WindowListener, ActionListe
 	JTextField localServerPortnum;
 	JLabel info;
 	JButton connect;
-	
+
 	final Object statusTextSync = new Object();
 	String statusText = "";
 
 	final Object buttonTextSync = new Object();
 	String buttonText = "Start";
-	
+
 	Thread serverMainThread = null;
-	
+
 	public JFrame main;
-	
+
+	MyPropertiesFile pf;
+
 	public CraftProxyGUI() {
+
+		pf = new MyPropertiesFile("CraftProxyLiteGUI.txt");
+
+		pf.load();
+		
+		String defaultHostname = pf.getString("connect_hostname", "localhost");
+		int defaultPort = pf.getInt("connect_port", 25565);
+		int listenPort = pf.getInt("listen_port", 25565);
 
 		setTitle("CraftProxyLite Local Cache Mode");
 		setSize(375,200);
 		setLocation(40,150);
-		
+
 
 		topPanel.setLayout(new BorderLayout());
 		secondPanel.setLayout(new BorderLayout());
-		
-		serverName = new JTextField("localhost", 20);
+
+		serverName = new JTextField(defaultHostname, 20);
 		TitledBorder border = new TitledBorder("Server Name");
 		serverName.setBorder(border);
-		
-		portNum = new JTextField("20000" , 6);
+		serverName.addActionListener(this);
+
+		portNum = new JTextField(Integer.toString(defaultPort) , 6);
 		border = new TitledBorder("Port");
 		portNum.setBorder(border);
-		
+		portNum.addActionListener(this);
+
 		localServerName = new JLabel("localhost");
 		border = new TitledBorder("Local Server Name");
 		localServerName.setBorder(border);
-		
-		localServerPortnum = new JTextField("25565", 6);
+
+		localServerPortnum = new JTextField(Integer.toString(listenPort), 6);
 		border = new TitledBorder("Port");
 		localServerPortnum.setBorder(border);
-		
+		localServerPortnum.addActionListener(this);
+
 		topPanel.add(serverName, BorderLayout.CENTER);
 		topPanel.add(portNum, BorderLayout.LINE_END);
-		
+
 		secondPanel.setLayout(new BorderLayout());
 		secondPanel.add(localServerName, BorderLayout.CENTER);
 		secondPanel.add(localServerPortnum, BorderLayout.LINE_END);
-		
+
 		combinedTop.setLayout(new BorderLayout());
 		combinedTop.add(topPanel, BorderLayout.CENTER);
 		combinedTop.add(secondPanel, BorderLayout.SOUTH);
-		
+
 		info = new JLabel();
 		border = new TitledBorder("Status");
 		info.setBorder(border);
-		
+
 		//SwingUtilities.invokeLater(paramRunnable);
-		
+
 		connect = new JButton(buttonText);
 		connect.addActionListener(this);
-		
+
 		setLayout(new BorderLayout());
-	    add(combinedTop, BorderLayout.PAGE_START);
-	    add(info, BorderLayout.CENTER);
-	    add(connect, BorderLayout.PAGE_END);
-	    
+		add(combinedTop, BorderLayout.PAGE_START);
+		add(info, BorderLayout.CENTER);
+		add(connect, BorderLayout.PAGE_END);
+
 		this.setResizable(false);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		
+
 		this.addWindowListener(this);
-		
+
 	}
-	
+
 	public void safeSetStatus(final String text) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -102,7 +115,7 @@ public class CraftProxyGUI extends JFrame implements WindowListener, ActionListe
 			}
 		});
 	}
-	
+
 	public void safeSetButton(final String text) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -125,7 +138,7 @@ public class CraftProxyGUI extends JFrame implements WindowListener, ActionListe
 	}
 
 	public void windowIconified(WindowEvent paramWindowEvent) {
-		
+
 	}
 
 	public void windowDeiconified(WindowEvent paramWindowEvent) {
@@ -139,18 +152,26 @@ public class CraftProxyGUI extends JFrame implements WindowListener, ActionListe
 
 	public void actionPerformed(ActionEvent action) {
 		if(action.getSource().equals(connect)) {
-			
+
+			try {			
+				pf.setString("connect_hostname", serverName.getText());
+				pf.setInt("connect_port", Integer.parseInt(portNum.getText()));
+				pf.setInt("listen_port", Integer.parseInt(localServerPortnum.getText()));
+				pf.save();
+			} catch (NumberFormatException nfe) {
+			}
+
 			if(serverMainThread == null || !serverMainThread.isAlive()) {
-				
+
 				safeSetButton("Stop");
-				
+
 				final String distantServer = serverName.getText() + ":" + portNum.getText();
 				final String localServer = localServerPortnum.getText();
-				
+
 				serverMainThread = new Thread(new Runnable() {
-					
+
 					public void run() {
-						
+
 						String[] args = {
 								localServer,
 								distantServer,
@@ -158,14 +179,14 @@ public class CraftProxyGUI extends JFrame implements WindowListener, ActionListe
 								"quiet",
 								"bridge_connection"
 						};
-						
+
 						Main.main(args, false);
 					}
-					
+
 				});
-				
+
 				serverMainThread.start();
-				
+
 			} else {
 				safeSetButton("Stopping");
 				serverMainThread.interrupt();
--- com/raphfrk/craftproxylite/VersionNumbering.java
+++ com/raphfrk/craftproxylite/VersionNumbering.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "40";
+	final static String version = "41";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
